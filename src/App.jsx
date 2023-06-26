e79552a9bc2e7b795b98cab280d8e27465b8da74
Francisco Garcia <=>
2023-06-26 15:29:47 -0600

    feat: configure routes app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
-import CommingSoon from './components/ComingSoon'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  return (
+    <div className="app">
+      <Header />
 
-  return <CommingSoon />
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/sites" element={<div>sites</div>} />
+        <Route path="/about" element={<div>about</div>} />
+        <Route path="/package" element={<div>package</div>} />
+        <Route path="/contact" element={<div>Contact</div>} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
