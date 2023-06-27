d02c559ddfb290a1042bee5fd16510e91152d69b
Francisco Garcia <=>
2023-06-27 15:26:46 -0600

    feat: render home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import './App.css'
 
+import HomePage from './pages/home'
 function App() {
   return (
     <div className="app">
       <Header />
 
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/sites" element={<div>sites</div>} />
         <Route path="/about" element={<div>about</div>} />
         <Route path="/package" element={<div>package</div>} />
