9f5ce5880688961fc638cb1c516ce106a29905de
Francisco Garcia <=>
2023-06-27 14:13:14 -0600

    feat: create navigation style

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+import './index.scss'
+
 export default function Navigation() {
   const { t } = useTranslation()
 
@@ -36,14 +38,31 @@ export default function Navigation() {
       <ul className="navigation__list">
         {NAVIGATION_ITEMS.map((item) => (
           <li key={item.id} className="navigation__item">
-            <NavLink to={item.path} className="navigation__link">
+            <NavLink
+              to={item.path}
+              className={({ isActive }) =>
+                isActive
+                  ? 'navigation__link navigation__link--active'
+                  : 'navigation__link'
+              }
+            >
               {t(item.name)}
             </NavLink>
           </li>
         ))}
-      </ul>
 
-      <h1>{t('navigation.home')}</h1>
+        <div className="content-language">
+          <span className="navigation__link language">
+            {t('navigation.language')}{' '}
+            <i className="fa-solid fa-globe-americas" />
+          </span>
+          <ul className="navigation__language">
+            <li>English</li>
+            <li>Español</li>
+            <li>Français</li>
+          </ul>
+        </div>
+      </ul>
     </nav>
   )
 }
