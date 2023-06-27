0798fa1b92b590641348a92ad40539053522bd2f
Francisco Garcia <=>
2023-06-27 15:26:26 -0600

    feat: create  home page

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,20 @@
+import Layout from '../../components/Layout'
+
+export default function HomePage() {
+  return (
+    <Layout>
+      <section className="home">
+        <h1>Home</h1>
+      </section>
+      <section className="recommended-sites">
+        <h2>Recommended Sites</h2>
+      </section>
+      <section className="services">
+        <h2>Services</h2>
+      </section>
+      <section className="testimonials">
+        <h2>Testimonials</h2>
+      </section>
+    </Layout>
+  )
+}
