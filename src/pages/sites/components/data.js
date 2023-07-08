b0edcb815a04d5f80510907d3aaa1807945f078e
Francisco Garcia <=>
2023-07-08 15:36:23 -0600

    fix: change location data

---
+++ src/pages/sites/components/data.js
@@ -0,0 +1,28 @@
+export const data = [
+  {
+    img: 'https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg',
+    country: 'Vietnam',
+    place: 'Ha Noi',
+    describe: 'Ha Noi is the capital of Vietnam'
+  },
+  {
+    img: 'https://ep01.epimg.net/elpais/imagenes/2019/10/30/album/1572424649_614672_1572453030_noticia_normal.jpg',
+    country: 'Alaska - USA',
+    place: 'Alaska',
+    describe: 'Alaska is the largest state in the United States by area'
+  },
+  {
+    img: 'https://st4.depositphotos.com/23862650/25152/i/450/depositphotos_251521566-stock-photo-flinged-disk-no1-at-badenweiler.jpg',
+    country: 'Germany',
+    place: 'Badenweiler',
+    describe:
+      'Badenweiler is a spa town in the district of Breisgau-Hochschwarzwald in Baden-Württemberg, Germany'
+  },
+  {
+    img: 'https://wwwhatsnew.com/wp-content/uploads/2022/10/Estos-son-los-10-mejores-sistemas-para-pasar-de-texto-a-imagen.jpg',
+    country: 'Denizli - Turkey',
+    place: 'Pamukkale',
+    describe:
+      'Pamukkale, meaning "cotton castle" in Turkish, is a natural site in Denizli in southwestern Turkey'
+  }
+]
--- src/pages/sites/index.jsx
+++ src/pages/sites/index.jsx
@@ -1,38 +1,11 @@
 import { useTranslation } from 'react-i18next'
 import { useRef, useEffect, useState } from 'react'
+import { data } from './components/data'
 import './index.scss'
-
-const data = [
-  {
-    img: 'https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg',
-    country: 'Vietnam',
-    place: 'Ha Noi',
-    describe: 'Ha Noi is the capital of Vietnam'
-  },
-  {
-    img: 'https://ep01.epimg.net/elpais/imagenes/2019/10/30/album/1572424649_614672_1572453030_noticia_normal.jpg',
-    country: 'Alaska - USA',
-    place: 'Alaska',
-    describe: 'Alaska is the largest state in the United States by area'
-  },
-  {
-    img: 'https://st4.depositphotos.com/23862650/25152/i/450/depositphotos_251521566-stock-photo-flinged-disk-no1-at-badenweiler.jpg',
-    country: 'Germany',
-    place: 'Badenweiler',
-    describe:
-      'Badenweiler is a spa town in the district of Breisgau-Hochschwarzwald in Baden-Württemberg, Germany'
-  },
-  {
-    img: 'https://wwwhatsnew.com/wp-content/uploads/2022/10/Estos-son-los-10-mejores-sistemas-para-pasar-de-texto-a-imagen.jpg',
-    country: 'Denizli - Turkey',
-    place: 'Pamukkale',
-    describe:
-      'Pamukkale, meaning "cotton castle" in Turkish, is a natural site in Denizli in southwestern Turkey'
-  }
-]
 
 export default function SitesPage() {
   const { t } = useTranslation()
+  const [sites, setSites] = useState(data)
   const introduce = useRef(null)
   const ordinalNumber = useRef(null)
   const thumbnailListWrapper = useRef(null)
@@ -44,18 +17,18 @@ export default function SitesPage() {
     ordinalNumber.current.innerHTML = ''
     thumbnailListWrapper.current.innerHTML = ''
 
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < sites.length; i++) {
       const introduceContent = document.createElement('div')
       introduceContent.classList.add('wrapper')
       introduceContent.innerHTML = `
           <span> 
-            <h5 class="country" style="--idx: 0">${data[i].country}</h5>
+            <h5 class="country" style="--idx: 0">${sites[i].country}</h5>
           </span>
           <span>
-            <h1 class="place" style="--idx: 1">${data[i].place}</h1>
+            <h1 class="place" style="--idx: 1">${sites[i].place}</h1>
           </span>
           <span>
-            <p class="describe" style="--idx: 2">${data[i].describe}</p>
+            <p class="describe" style="--idx: 2">${sites[i].describe}</p>
           </span>
           <span>
             <button class="discover-button" style="--idx: 3">Discover Now</button>
@@ -75,17 +48,17 @@ export default function SitesPage() {
     const thumbnailZoom = document.createElement('div')
     thumbnailZoom.classList.add('thumbnail', 'zoom')
     const thumbnailZoomImg = document.createElement('img')
-    thumbnailZoomImg.src = data[0].img
+    thumbnailZoomImg.src = sites[0].img
     thumbnailZoomImg.alt = 'thumbnail'
     thumbnailZoom.appendChild(thumbnailZoomImg)
     thumbnailListWrapperContent.appendChild(thumbnailZoom)
 
-    for (let i = 1; i < data.length; i++) {
+    for (let i = 1; i < sites.length; i++) {
       const thumbnail = document.createElement('div')
       thumbnail.classList.add('thumbnail')
       thumbnail.style.setProperty('--idx', i - 1)
       const thumbnailImg = document.createElement('img')
-      thumbnailImg.src = data[i].img
+      thumbnailImg.src = sites[i].img
       thumbnailImg.alt = 'thumbnail'
       thumbnail.appendChild(thumbnailImg)
       thumbnailListWrapperContent.appendChild(thumbnail)
@@ -117,7 +90,7 @@ export default function SitesPage() {
 
     // Actualizar el estado del currentIndex
     setCurrentIndex((prevIndex) => {
-      if (prevIndex < data.length - 1) {
+      if (prevIndex < sites.length - 1) {
         return prevIndex + 1
       } else {
         return 0
@@ -125,7 +98,7 @@ export default function SitesPage() {
     })
 
     // Remover la clase 'active' de todos los elementos
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < sites.length; i++) {
       introduce.current.children[i].classList.remove('active')
       ordinalNumber.current.children[i].classList.remove('active')
     }
