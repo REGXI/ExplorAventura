06de2dd3b2a9f167b8c57ddfcc38da473bf29705
Francisco Garcia <=>
2023-07-13 11:38:08 -0600

    feat:  tracket data

---
+++ src/data/valuesData.js
@@ -0,0 +1,38 @@
+import teamWorkIcon from '../assets/svg/teamwork.svg'
+import integrityIcon from '../assets/svg/integrity.svg'
+import passionIcon from '../assets/svg/passion.svg'
+import commitmentIcon from '../assets/svg/commitment.svg'
+import learningIcon from '../assets/svg/learning.svg'
+
+export const valuesData = [
+  {
+    id: 1,
+    title: 'about.integrity',
+    description: 'about.integrity-description',
+    icon: integrityIcon
+  },
+  {
+    id: 2,
+    title: 'about.passion',
+    description: 'about.passion-description',
+    icon: passionIcon
+  },
+  {
+    id: 3,
+    title: 'about.team-work',
+    description: 'about.team-work-description',
+    icon: teamWorkIcon
+  },
+  {
+    id: 4,
+    title: 'about.commitment',
+    description: 'about.commitment-description',
+    icon: commitmentIcon
+  },
+  {
+    id: 5,
+    title: 'about.learning',
+    description: 'about.learning-description',
+    icon: learningIcon
+  }
+]
