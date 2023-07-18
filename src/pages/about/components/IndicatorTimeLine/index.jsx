f2371a5f534f377b619f23f4c65d5cd46d7dfbe2
Francisco Garcia <=>
2023-07-18 11:48:16 -0600

    feat: create indicatortimeline component

---
+++ src/pages/about/components/IndicatorTimeLine/index.jsx
@@ -0,0 +1,10 @@
+import './index.scss'
+export function IndicatorTimeLine() {
+  return (
+    <div className="indicator">
+      <i class="fas fa-caret-left"></i>
+      <div className="caret-line"></div>
+      <i class="fas fa-caret-right"></i>
+    </div>
+  )
+}
