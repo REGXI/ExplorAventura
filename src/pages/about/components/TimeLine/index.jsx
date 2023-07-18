d34217376865289afaf4688c28a6a78987e595e4
Francisco Garcia <=>
2023-07-18 11:48:46 -0600

    fix: implement timeline component

--- src/pages/about/components/TimeLine/index.jsx
+++ src/pages/about/components/TimeLine/index.jsx
@@ -1,13 +1,57 @@
+import './index.scss'
 import LayoutPage from '../../../../components/LayuotPage'
+import { InfoTimeLine } from '../InfoTimeLine'
+import { DateTimeLine } from '../DateTimeLine'
+import { IndicatorTimeLine } from '../IndicatorTimeLine'
 export function TimeLine({ t }) {
   return (
     <LayoutPage
-      title="about:timeline.title"
-      subject="about:timeline.subject"
-      titleColor="blue"
+      title="about.timeline-title"
+      subject="about.timeline-subject"
       t={t}
     >
-      <div className="timeline"></div>
+      <div className="timeline-content">
+        <section className="left-info">
+          {/*  */}
+          <InfoTimeLine
+            title="title"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis"
+          />
+          <DateTimeLine date="2019" />
+
+          <InfoTimeLine
+            title="title"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis"
+          />
+          <DateTimeLine date="2019" />
+
+          {/*  */}
+        </section>
+        <section className="draw-timeline">
+          <div className="line"> </div>
+          {/*  */}
+          <IndicatorTimeLine />
+          <IndicatorTimeLine />
+          <IndicatorTimeLine />
+          <IndicatorTimeLine />
+          {/*  */}
+        </section>
+        <section className="right-info">
+          {/*  */}
+          <DateTimeLine date="2019" />
+
+          <InfoTimeLine
+            title="title"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis"
+          />
+          <DateTimeLine date="2019" />
+          <InfoTimeLine
+            title="title"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis"
+          />
+          {/*  */}
+        </section>
+      </div>
     </LayoutPage>
   )
 }
