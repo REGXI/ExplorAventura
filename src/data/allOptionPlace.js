aebe2d0fc7c0c95f174df8e5b4c7f3cb03dc961e
Francisco Garcia <=>
2023-07-25 09:38:44 -0600

    feat: array data to places

---
+++ src/data/allOptionPlace.js
@@ -0,0 +1,30 @@
+export const optionsPlaces = [
+  { value: 'managua', label: 'Managua' },
+  { value: 'granada', label: 'Granada' },
+  { value: 'leon', label: 'Leon' },
+  { value: 'masaya', label: 'Masaya' },
+  { value: 'matagalpa', label: 'Matagalpa' },
+  { value: 'rivas', label: 'Rivas' },
+  { value: 'carazo', label: 'Carazo' },
+  { value: 'jinotega', label: 'Jinotega' },
+  { value: 'chinandega', label: 'Chinandega' },
+  { value: 'esteli', label: 'Esteli' },
+  { value: 'chontales', label: 'Chontales' },
+  { value: 'boaco', label: 'Boaco' },
+  { value: 'nueva-segovia', label: 'Nueva Segovia' },
+  { value: 'rio-san-juan', label: 'Rio San Juan' },
+  { value: 'madriz', label: 'Madriz' },
+  { value: 'atlantico-norte', label: 'Atlantico Norte' },
+  { value: 'atlantico-sur', label: 'Atlantico Sur' }
+]
+
+export const durationOptions = [
+  { value: '1', label: '1 día' },
+  { value: '2', label: '2 días' },
+  { value: '3', label: '3 días' },
+  { value: '4', label: '4 días' },
+  { value: '5', label: '5 días' },
+  { value: '6', label: '6 días' },
+  { value: '7', label: '7 días' },
+  { value: '8', label: 'Más de una semana' }
+]
---
+++ src/data/places.js
@@ -0,0 +1,140 @@
+export const places = [
+  {
+    destination: 'leon',
+    places: [
+      {
+        name: 'Catedral de León',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'La majestuosa Catedral de la Asunción de León es una joya arquitectónica que te dejará maravillado. Su imponente estructura neoclásica y su rica historia como la catedral más grande de América Central hacen de esta visita una experiencia inolvidable.'
+      },
+      {
+        name: 'Ruinas de León Viejo',
+        image: 'https://placehold.co/600x400.png',
+        description: 'Ruinas de León Viejo'
+      },
+      {
+        name: 'Museo de Arte Fundación Ortiz-Gurdián',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Adéntrate en el vibrante mundo del arte contemporáneo y moderno en este museo, donde una ecléctica colección de obras de artistas locales e internacionales te cautivará con su creatividad.'
+      },
+      {
+        name: 'Parque Central de León',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'El corazón latente de la ciudad, el Parque Central, es el lugar perfecto para empaparse del ambiente local. Rodeado de edificios históricos y coloridos, es ideal para sentarse, relajarse y disfrutar de la atmósfera única de León.'
+      },
+      {
+        name: 'Volcán Cerro Negro',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Para los amantes de la aventura, el volcán activo Cerro Negro es una visita obligada. Camina por sus laderas negras y desafía la gravedad practicando sandboarding, ¡una experiencia única que nunca olvidarás!'
+      },
+      {
+        name: 'Playa Las Peñitas',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Escapa del bullicio de la ciudad y disfruta de la tranquilidad en Playa Las Peñitas. Sus aguas cálidas y su arena dorada te invitan a relajarte y deleitarte con la belleza natural que ofrece la costa del Pacífico.'
+      }
+    ]
+  },
+  {
+    destination: 'managua',
+    places: [
+      {
+        name: 'Puerto Salvador Allende',
+        image: 'https://placehold.co/600x400.png',
+        description: ''
+      },
+      {
+        name: 'Mercado Roberto Huembes',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Como uno de los mercados más grandes de Managua, el Mercado Roberto Huembes atrae a visitantes que buscan sumergirse en la cultura nicaragüense. Aquí encontrarás una amplia variedad de productos, desde artesanías y recuerdos hasta alimentos frescos y productos locales.'
+      },
+      {
+        name: 'Puerto de Asese',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Con sus pintorescas casas sobre pilotes en el Lago Xolotlán, el Puerto de Asese atrae a visitantes en busca de experiencias únicas y fotografías memorables.'
+      },
+      {
+        name: 'Galería-Café Sébaco',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Para los amantes del arte y la cultura, este espacio cultural ofrece una oportunidad para apreciar el talento artístico local mientras disfrutas de un delicioso café nicaragüense.'
+      }
+    ]
+  },
+  {
+    destination: 'granada',
+    places: [
+      {
+        name: 'Isletas de Granada',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Un paseo en bote por las Isletas de Granada es una experiencia imperdible. Estas pequeñas islas en el Lago Nicaragua están llenas de exuberante vegetación y vida silvestre. Puedes disfrutar de un tranquilo paseo en bote o incluso tomar un baño en el lago.'
+      },
+      {
+        name: 'Museo de Chocolate:',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Para los amantes del chocolate, este museo es un lugar fascinante para aprender sobre el proceso de fabricación del chocolate, su historia y disfrutar de degustaciones.'
+      },
+      {
+        name: 'Parque Central de Granada',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'El corazón de la ciudad, este encantador parque es el lugar perfecto para disfrutar del ambiente local, relajarse y admirar la arquitectura colonial que lo rodea. Es un lugar popular para sentarse y disfrutar de la vida cotidiana de la ciudad.'
+      },
+      {
+        name: 'Mirador de La Merced',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Ubicado en el campanario de la Iglesia de La Merced, este mirador ofrece vistas panorámicas impresionantes de la ciudad, el Lago Nicaragua y el Volcán Mombacho. Es un lugar popular para tomar fotografías y disfrutar de las vistas.'
+      },
+      {
+        name: 'Fortaleza La Pólvora',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Esta antigua fortaleza es un sitio histórico que data del siglo XVIII y ofrece una vista panorámica de la ciudad desde su mirador. Es un lugar interesante para aprender sobre la historia militar de Granada.'
+      }
+    ]
+  },
+  {
+    destination: 'rivas',
+    places: [
+      {
+        name: 'Isla de Ometepe',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Esta isla es una joya natural en el Lago de Nicaragua y es famosa por sus dos volcanes gemelos, Concepción y Maderas. Los visitantes pueden disfrutar de actividades como senderismo, observación de vida silvestre y visitas a comunidades locales.'
+      },
+      {
+        name: 'Playa Gigante',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Una playa hermosa y relajante que atrae a surfistas y turistas que buscan tranquilidad y bellos paisajes. Es un lugar ideal para practicar surf, tomar el sol o disfrutar de paseos en bote.'
+      },
+      {
+        name: 'San Juan del Sur',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'A unos 20 kilómetros de Rivas, esta pintoresca ciudad costera es famosa por sus playas, su animada vida nocturna y su ambiente relajado. Es un destino popular para los surfistas y los amantes de la playa.'
+      },
+      {
+        name: 'Reserva Biológica Indio-Maíz',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Ubicada en la región de Rivas, esta reserva natural es una de las áreas de selva tropical más grandes y mejor conservadas de Nicaragua. Ofrece una gran biodiversidad y es un lugar ideal para los amantes de la naturaleza y la observación de aves.'
+      },
+      {
+        name: 'Reserva Natural Los Guatuzos',
+        image: 'https://placehold.co/600x400.png',
+        description:
+          'Otra reserva natural en la región de Rivas, Los Guatuzos ofrece la oportunidad de explorar los humedales y los canales del Río San Juan, y es un lugar ideal para avistamiento de vida silvestre.'
+      }
+    ]
+  }
+]
