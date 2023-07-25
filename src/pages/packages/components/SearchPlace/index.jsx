fe240caa1490c879758b110ff2151e12d19bf70a
Francisco Garcia <=>
2023-07-25 09:40:58 -0600

    feat: search places component is ready

---
+++ src/pages/packages/components/SearchPlace/index.jsx
@@ -0,0 +1,84 @@
+import { optionsPlaces, durationOptions } from '../../../../data/allOptionPlace'
+import { useForm } from '../../../../hooks/useForm'
+import { getPlacesByDestination } from '../../../../services/getPlaces'
+export function SearchPlaces({ setPlaces }) {
+  const initialValues = {
+    destination: 'all',
+    period: '',
+    duration: '1',
+    travellers: ''
+  }
+
+  const [values, handleInputChange, reset, handleSelectChange] =
+    useForm(initialValues)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const { destination } = values
+    setPlaces(getPlacesByDestination(destination))
+  }
+
+  return (
+    <form className="seach-place" onSubmit={handleSubmit}>
+      <div className="destination">
+        <label htmlFor="destination">Destination</label>
+        <select
+          name="destination"
+          id="destination"
+          value={values.destination}
+          onChange={handleSelectChange}
+        >
+          <option value="all">All</option>
+          {optionsPlaces?.map((option, index) => (
+            <option key={index} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="period">
+        <label htmlFor="period">Period</label>
+        <input
+          type="date"
+          name="period"
+          id="period"
+          placeholder="Period"
+          onChange={handleInputChange}
+          value={values.period}
+        />
+      </div>
+      <div className="duration">
+        <label htmlFor="duration">Duration</label>
+        <select
+          name="duration"
+          id="duration"
+          value={values.duration}
+          onChange={handleSelectChange}
+        >
+          {durationOptions?.map((option, index) => {
+            return (
+              <option key={index} value={option.value}>
+                {option.label}
+              </option>
+            )
+          })}
+        </select>
+      </div>
+      <div className="travellers">
+        <label htmlFor="travellers">Travellers</label>
+        <input
+          type="number"
+          name="travellers"
+          id="travellers"
+          placeholder="0"
+          onChange={handleInputChange}
+          value={values.travellers}
+        />
+      </div>
+
+      <div className="button">
+        <button type="submit">Search</button>
+      </div>
+    </form>
+  )
+}
