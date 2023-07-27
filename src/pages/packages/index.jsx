5f5bf9c8158c1d5bebf60f6d3178f9cf58d97731
Francisco Garcia <=>
2023-07-27 14:57:27 -0600

    feat: render random image

--- src/pages/packages/index.jsx
+++ src/pages/packages/index.jsx
@@ -2,13 +2,15 @@ import './index.scss'
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { SearchPlaces } from './components/SearchPlace'
-import { getAllPlaces, getPlacesByDestination } from '../../services/getPlaces'
+import { getAllPlaces, getRamdonDestinations } from '../../services/getPlaces'
 import { RenderPlaces } from './components/RenderPlaces'
 import { FocusedItemPlace } from './components/FocusedItemPlace'
+import { CustomPackage } from './components/CustomPackage'
 export default function PackagesPage() {
   const { t } = useTranslation()
   const [places, setPlaces] = useState(getAllPlaces())
   const [focusedPlace, setFocusedPlace] = useState({})
+  const [randomDestination, setRamdonDestination] = useState({})
 
   const handleClickedPlace = (place) => setFocusedPlace({ ...place })
 
@@ -16,32 +18,42 @@ export default function PackagesPage() {
     setFocusedPlace(places[0])
   }, [places])
 
+  useEffect(() => {
+    setRamdonDestination(getRamdonDestinations())
+  }, [])
+
   return (
     <div className="packages-page">
-      <section className="information-package">
-        <div className="title">
-          <h1>Buy Vacation Packages at the Best Prices</h1>
-          <p>Explore Best Selling Package</p>
+      <section className="package">
+        <section className="information-package">
+          <div className="title">
+            <h1>Buy Vacation Packages at the Best Prices</h1>
+            <p>Explore Best Selling Package</p>
 
-          <button>
-            <span>Contact with Agent</span>
-          </button>
-        </div>
+            <button>
+              <span>Contact with Agent</span>
+            </button>
+          </div>
 
-        <div className="search-package">
-          <SearchPlaces setPlaces={setPlaces} />
-        </div>
+          <div className="search-package">
+            <SearchPlaces setPlaces={setPlaces} />
+          </div>
 
-        <div className="related-places">
-          <RenderPlaces
-            places={places}
-            t={t}
-            handleClickedPlace={handleClickedPlace}
-          />
-        </div>
+          <div className="related-places">
+            <RenderPlaces
+              places={places}
+              t={t}
+              handleClickedPlace={handleClickedPlace}
+            />
+          </div>
+        </section>
+        <section className="package-container">
+          <FocusedItemPlace focusedPlace={focusedPlace} />
+        </section>
       </section>
-      <section className="package-container">
-        <FocusedItemPlace focusedPlace={focusedPlace} />
+
+      <section className="custom-packages">
+        <CustomPackage randomDestination={randomDestination} />
       </section>
     </div>
   )
