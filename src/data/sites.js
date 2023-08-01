048d7285a37196f229a31380f38cae0229f7daf5
Francisco Garcia <=>
2023-08-01 16:27:13 -0600

    refact: rename file

---
+++ src/data/sites.js
@@ -0,0 +1,34 @@
+import site_granada from '../assets/images/site_granada.jpg'
+import Ometepe from '../assets/images/ometepe.webp'
+import cerronegro from '../assets/images/cerronegro22.jpg'
+import atlanticoNorte from '../assets/images/atlantico_norte.jpg'
+export const sites = [
+  {
+    img: site_granada,
+    country: 'Granada - Nicaragua',
+    place: 'Granada',
+    describe:
+      'Granada, colonial city in Nicaragua. Cobbled streets, historic architecture and a vibrant atmosphere. Enjoy its charm and culture!'
+  },
+  {
+    img: Ometepe,
+    country: 'Rivas - Nicaragua',
+    place: 'Isla de Ometepe',
+    describe:
+      'Explore the mystical Isla de Ometepe in Nicaragua, home to two majestic volcanoes and exuberant nature. Amazing!'
+  },
+  {
+    img: cerronegro,
+    country: 'Leon - Nicaragua',
+    place: 'Volcan Cerro Negro',
+    describe:
+      'Experimenta la emoción del "Volcano Boarding" en el activo Cerro Negro, Nicaragua. ¡Desciende sus laderas negras a toda velocidad!'
+  },
+  {
+    img: atlanticoNorte,
+    country: 'Atlantico Norte - Nicaragua',
+    place: 'Atlantico Norte',
+    describe:
+      'Golden beaches, warm Caribbean Sea, rich culture, exuberant nature, adventure and hospitality in Nicaragua. Find out now!'
+  }
+]
