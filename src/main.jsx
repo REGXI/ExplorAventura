6ac42b7bf688540b4d13e3cd597e1908c473be91
Francisco Garcia <=>
2023-08-02 10:32:41 -0600

    feat: configure global state with redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { store } from './store/index.js'
+import { Provider } from 'react-redux'
 import './index.css'
 
 import { I18nextProvider } from 'react-i18next'
@@ -9,10 +11,12 @@ import i18n from './i18n.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </I18nextProvider>
+    <Provider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </I18nextProvider>
+    </Provider>
   </React.StrictMode>
 )
