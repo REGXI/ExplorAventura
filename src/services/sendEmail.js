f0519332231a2f654c6e4ff4bef95974c7a7e87d
Francisco Garcia <=>
2023-08-07 13:49:54 -0600

    feat: function to convert object to form html

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -5,9 +5,22 @@ const userID = 'hRPW11MfeZdHDW_1j'
 
 const TEMPLATESID = {
   contact: 'template_zqodkd7',
-  package: 'template_1g2g7t6'
+  package: 'template_yze9mzc'
 }
 
 export const sendEmail = async (template, form) => {
   return await sendForm(serviceID, TEMPLATESID[template], form, userID)
+}
+
+export const converObjectToHtmlForm = (data) => {
+  const form = document.createElement('form')
+
+  for (const key in data) {
+    const input = document.createElement('input')
+    input.name = key
+    input.value = data[key]
+    form.appendChild(input)
+  }
+
+  return form
 }
