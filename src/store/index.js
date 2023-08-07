25ea7fecf5fd684bdaf4b6ef9ac9a97ef38b94e6
Francisco Garcia <=>
2023-08-07 10:57:39 -0600

    feat: create store to search place form slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import navigationColorReducer from './features/navigationColorSlice'
+import searchPlaceSlice from './features/searchPlaceSlice'
 
 export const store = configureStore({
   reducer: {
-    navigationColor: navigationColorReducer
+    navigationColor: navigationColorReducer,
+    searchPlace: searchPlaceSlice
   }
 })
