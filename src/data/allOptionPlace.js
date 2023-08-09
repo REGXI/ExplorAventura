f157566df1648bdb34b81233cc6dcee043259208
Francisco Garcia <=>
2023-08-09 10:26:47 -0600

    fix: new destination optionions

--- src/data/allOptionPlace.js
+++ src/data/allOptionPlace.js
@@ -1,22 +1,45 @@
 export const optionsPlaces = [
-  { value: 'managua', label: 'Managua' },
-  { value: 'granada', label: 'Granada' },
-  { value: 'leon', label: 'Leon' },
-  { value: 'masaya', label: 'Masaya' },
-  { value: 'matagalpa', label: 'Matagalpa' },
-  { value: 'rivas', label: 'Rivas' },
-  { value: 'carazo', label: 'Carazo' },
-  { value: 'jinotega', label: 'Jinotega' },
-  { value: 'chinandega', label: 'Chinandega' },
-  { value: 'esteli', label: 'Esteli' },
-  { value: 'chontales', label: 'Chontales' },
-  { value: 'boaco', label: 'Boaco' },
-  { value: 'nueva-segovia', label: 'Nueva Segovia' },
-  { value: 'rio-san-juan', label: 'Rio San Juan' },
-  { value: 'madriz', label: 'Madriz' },
-  { value: 'atlantico-norte', label: 'Atlantico Norte' },
-  { value: 'atlantico-sur', label: 'Atlantico Sur' }
+  { value: 'Managua', label: 'Managua' },
+  { value: 'Granada', label: 'Granada / Isletas' },
+  { value: 'Leon', label: 'Leon' },
+  { value: 'Masaya / Volcan Masaya', label: 'Masaya / Volcan Masaya' },
+  { value: 'Matagalpa', label: 'Matagalpa' },
+  {
+    value: 'Chinandega / Fabrica Ron Flor de Caña',
+    label: 'Chinandega / Fabrica Ron Flor de Caña'
+  },
+  {
+    value: 'Rivas / Isla de Ometepe',
+    label: 'Rivas / Isla de Ometepe'
+  },
+  {
+    value: 'Esteli / Fabricas de Tabaco',
+    label: 'Esteli / Fabricas de Tabaco'
+  },
+  {
+    value: 'Laguna de Perlas / Cayos Perlas',
+    label: 'Laguna de Perlas / Cayos Perlas'
+  },
+  {
+    value: 'Corn Island / Little Corn Island',
+    label: 'Corn Island / Little Corn Island'
+  },
+  { value: 'San Juan del Sur / Playas', label: 'San Juan del Sur / Playas' }
 ]
+
+/**
+-	Managua
+-	Granada / Isletas
+-	Leon
+-	Masaya / Volcan Masaya
+-	Matagalpa
+-	Chinandega / Fabrica Ron Flor de Caña
+-	Esteli / Fabricas de Tabaco
+-	Rio San Juan / Solentiname
+-	Corn Island / Little Corn Island
+-	Laguna de Perlas / Cayos Perlas
+-	San Juan del Sur / Playas
+ */
 
 export const durationOptions = [
   { value: '1', label: 'day' },
@@ -25,6 +48,5 @@ export const durationOptions = [
   { value: '4', label: 'days' },
   { value: '5', label: 'days' },
   { value: '6', label: 'days' },
-  { value: '7', label: 'days' },
-  { value: '8', label: 'More than a week' }
+  { value: '7', label: 'days' }
 ]
