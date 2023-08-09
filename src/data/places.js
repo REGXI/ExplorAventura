2b30715c47131ae9be9ae9ed75e63db091e3f426
Francisco Garcia <=>
2023-08-09 10:27:05 -0600

    fix: render new places

--- src/data/places.js
+++ src/data/places.js
@@ -1,77 +1,94 @@
 import granada_isletas_granada from '../assets/images/granada_isletas_granada.webp'
 import granada_parque_central from '../assets/images/granada_parque_central.webp'
 import granada_mirador_merced from '../assets/images/granada_mirador_merced.webp'
-import granada_fortaleza_polvora from '../assets/images/granada_fortaleza_polvora.webp'
+// import granada_fortaleza_polvora from '../assets/images/granada_fortaleza_polvora.webp'
 
-import rivas_isla_de_ometepe from '../assets/images/rivas_isla_de_ometepe.webp'
-import rivas_playa_gigante from '../assets/images/rivas_playa_gigante.webp'
-import rivas_san_juan_del_sur from '../assets/images/rivas_san_juan_del_sur.webp'
-import rivas_reserva_indio_maiz from '../assets/images/rivas_reserva_indio_maiz.webp'
-import rivas_los_guatuzos from '../assets/images/rivas_los_guatuzo.webp'
+import leon_catedral from '../assets/images/catedral_de_leon.webp'
+import leon_ruinas_leon_viejo from '../assets/images/ruinas_leon_viejo.webp'
+import leon_cerronegro from '../assets/images/volcan_cerronegro.webp'
+import leon_peñitas from '../assets/images/las_peñitas.jpg'
+
+import managua_puerto_allende from '../assets/images/puerto_allende.webp'
+
+import masaya_volcan_masaya from '../assets/images/volcan_masaya.webp'
+import masaya_volcan_masaya2 from '../assets/images/volcan_masaya2.webp'
+
+import matagalpa from '../assets/images/matagalpa.webp'
+
+import estieli_fabrica_tabaco from '../assets/images/fabrica_tabaco.webp'
+
+import cayo_perlas from '../assets/images/cayo_perla.webp'
+import cayo_perlas2 from '../assets/images/cayo_perla2.webp'
+
+import chinandega_fabrica_flor_de_caña from '../assets/images/tour-flor-de-Caña.webp'
+
+import corn_island from '../assets/images/cornisland.webp'
+import corn_island2 from '../assets/images/cornisland2.webp'
+
+import rivas_isla_de_ometepe from '../assets/images/isla_ometepe.webp'
+// import rivas_playa_gigante from '../assets/images/rivas_playa_gigante.webp'
+import rivas_san_juan_del_sur from '../assets/images/san_juan_delsur.webp'
+// import rivas_reserva_indio_maiz from '../assets/images/rivas_reserva_indio_maiz.webp'
+// import rivas_los_guatuzos from '../assets/images/rivas_los_guatuzo.webp'
 
 export const places = [
   {
-    destination: 'leon',
+    destination: 'Leon',
     places: [
       {
         name: 'Catedral de León',
-        image: 'https://placehold.co/600x400.png',
+        image: leon_catedral,
         description: 'Catedral de León'
       },
       {
         name: 'Ruinas de León Viejo',
-        image: 'https://placehold.co/600x400.png',
+        image: leon_ruinas_leon_viejo,
         description: 'Ruinas de León Viejo'
       },
-      {
-        name: 'Museo de Arte Fundación Ortiz-Gurdián',
-        image: 'https://placehold.co/600x400.png',
-        description: 'Museo de Arte Fundación Ortiz-Gurdián'
-      },
-      {
-        name: 'Parque Central de León',
-        image: 'https://placehold.co/600x400.png',
-        description: 'Parque Central de León'
-      },
+      // {
+      //   name: 'Parque Central de León',
+      //   image: 'https://placehold.co/600x400.png',
+      //   description: 'Parque Central de León'
+      // },
       {
         name: 'Volcán Cerro Negro',
-        image: 'https://placehold.co/600x400.png',
+        image: leon_cerronegro,
         description: 'Volcán Cerro Negro'
       },
       {
         name: 'Playa Las Peñitas',
-        image: 'https://placehold.co/600x400.png',
+        image: leon_peñitas,
         description: 'Playa Las Peñitas'
       }
     ]
   },
   {
-    destination: 'managua',
+    destination: 'Managua',
     places: [
       {
         name: 'Puerto Salvador Allende',
-        image: 'https://placehold.co/600x400.png',
+        image: managua_puerto_allende,
         description: 'Puerto Salvador Allende'
-      },
-      {
-        name: 'Mercado Roberto Huembes',
-        image: 'https://placehold.co/600x400.png',
-        description: 'Mercado Roberto Huembes'
-      },
-      {
-        name: 'Puerto de Asese',
-        image: 'https://placehold.co/600x400.png',
-        description: 'Puerto de Asese'
-      },
-      {
-        name: 'Galería-Café Sébaco',
-        image: 'https://placehold.co/600x400.png',
-        description: 'Galería-Café Sébaco'
       }
+      // {
+      //   name: 'Mercado Roberto Huembes',
+      //   image: 'https://placehold.co/600x400.png',
+      //   description: 'Mercado Roberto Huembes'
+      // },
+      // {
+      //   name: 'Puerto de Asese',
+      //   image: 'https://placehold.co/600x400.png',
+      //   description: 'Puerto de Asese'
+      // },
+      // {
+      //   name: 'Galería-Café Sébaco',
+      //   image: 'https://placehold.co/600x400.png',
+      //   description: 'Galería-Café Sébaco'
+      // }
     ]
   },
   {
-    destination: 'granada',
+    destination: 'Granada',
     places: [
       {
         name: 'Isletas de Granada',
@@ -82,46 +99,116 @@ export const places = [
         name: 'Parque Central de Granada',
         image: granada_parque_central,
         description: 'Parque Central de Granada'
-      },
-      {
-        name: 'Mirador de La Merced',
-        image: granada_mirador_merced,
-        description: 'Mirador de La Merced'
-      },
-      {
-        name: 'Fortaleza La Pólvora',
-        image: granada_fortaleza_polvora,
-        description: 'Fortaleza La Pólvora'
       }
+      // {
+      //   name: 'Mirador de La Merced',
+      //   image: granada_mirador_merced,
+      //   description: 'Mirador de La Merced'
+      // }
+      // {
+      //   name: 'Fortaleza La Pólvora',
+      //   image: granada_fortaleza_polvora,
+      //   description: 'Fortaleza La Pólvora'
+      // }
     ]
   },
   {
-    destination: 'rivas',
+    destination: 'Rivas / Isla de Ometepe',
     places: [
       {
         name: 'Isla de Ometepe',
         image: rivas_isla_de_ometepe,
         description: 'Isla de Ometepe'
-      },
-      {
-        name: 'Playa Gigante',
-        image: rivas_playa_gigante,
-        description: 'Playa Gigante'
-      },
+      }
+      // {
+      //   name: 'Reserva Biológica Indio-Maíz',
+      //   image: rivas_reserva_indio_maiz,
+      //   description: 'Reserva Biológica Indio-Maíz'
+      // },
+    ]
+  },
+  {
+    destination: 'San Juan del Sur / Playas',
+    places: [
       {
         name: 'San Juan del Sur',
         image: rivas_san_juan_del_sur,
         description: 'San Juan del Sur'
+      }
+    ]
+  },
+  {
+    destination: 'Masaya / Volcan Masaya',
+    places: [
+      {
+        name: 'Volcán Masaya',
+        image: masaya_volcan_masaya,
+        description: 'Volcán Masaya'
       },
       {
-        name: 'Reserva Biológica Indio-Maíz',
-        image: rivas_reserva_indio_maiz,
-        description: 'Reserva Biológica Indio-Maíz'
+        name: 'Volcán Masaya',
+        image: masaya_volcan_masaya2,
+        description: 'Volcán Masaya'
+      }
+    ]
+  },
+  {
+    destination: 'Matagalpa',
+    places: [
+      {
+        name: 'Matagalpa',
+        image: matagalpa,
+        description: 'Matagalpa'
+      }
+    ]
+  },
+  {
+    destination: 'Esteli / Fabricas de Tabaco',
+    places: [
+      {
+        name: 'Esteli',
+        image: estieli_fabrica_tabaco,
+        description: 'Esteli'
+      }
+    ]
+  },
+  {
+    destination: 'Laguna de Perlas / Cayos Perlas',
+    places: [
+      {
+        name: 'Laguna de Perlas',
+        image: cayo_perlas2,
+        description: 'Laguna de Perlas'
+      }
+      // {
+      //   name: 'Laguna de Perlas',
+      //   image: cayo_perlas,
+      //   description: 'Laguna de Perlas'
+      // }
+    ]
+  },
+  {
+    destination: 'Chinandega / Fabrica Ron Flor de Caña',
+    places: [
+      {
+        name: 'Fabrica Ron Flor de Caña',
+        image: chinandega_fabrica_flor_de_caña,
+        description: 'Chinandega'
+      }
+    ]
+  },
+  {
+    destination: 'Corn Island / Little Corn Island',
+    places: [
+      {
+        name: 'Corn Island',
+        image: corn_island,
+        description: 'Corn Island'
       },
       {
-        name: 'Reserva Natural Los Guatuzos',
-        image: rivas_los_guatuzos,
-        description: 'Reserva Natural Los Guatuzos'
+        name: 'Corn Island',
+        image: corn_island2,
+        description: 'Corn Island'
       }
     ]
   }
