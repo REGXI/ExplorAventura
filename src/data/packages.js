2fa7cb05261528a484999a7efbda8df6f3ea0290
Francisco Garcia <=>
2023-08-15 16:05:12 -0600

    feat: Package items example

---
+++ src/data/packages.js
@@ -0,0 +1,88 @@
+export const PACKAGE_ITEMS = [
+  {
+    id: 1,
+    title: 'Basic',
+    image: 'https://picsum.photos/600/800',
+    trajectory: [
+      {
+        title: 'day 1',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod',
+        site: 'Granada',
+        image: 'https://picsum.photos/600/800'
+      },
+      {
+        title: 'day 2',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod',
+        site: 'Granada',
+        image: 'https://picsum.photos/600/800'
+      },
+      {
+        title: 'day 3',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod',
+        site: 'Granada',
+        image: 'https://picsum.photos/600/800'
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: 'Medium',
+    image: 'https://picsum.photos/600/800',
+    trajectory: [
+      {
+        title: 'day 1',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod',
+        site: 'leon',
+        image: 'https://picsum.photos/600/800'
+      },
+      {
+        title: 'day 2',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod',
+        site: 'leon',
+        image: 'https://picsum.photos/600/800'
+      },
+      {
+        title: 'day 3',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod',
+        site: 'leon',
+        image: 'https://picsum.photos/600/800'
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: 'Premium',
+    image: 'https://picsum.photos/600/800',
+    trajectory: [
+      {
+        title: 'day 1',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod',
+        site: 'Managua',
+        image: 'https://picsum.photos/600/800'
+      },
+      {
+        title: 'day 2',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod',
+        site: 'Managua',
+        image: 'https://picsum.photos/600/800'
+      },
+      {
+        title: 'day 3',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae diam euismod',
+        site: 'Managua',
+        image: 'https://picsum.photos/600/800'
+      }
+    ]
+  }
+]
+
+export const getPackageItems = () => PACKAGE_ITEMS
