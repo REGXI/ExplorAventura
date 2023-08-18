c29f5a09ab5b8ee4495b201f58e4fa8a7ce514b5
Francisco Garcia <=>
2023-08-18 16:26:02 -0600

    fix: using translation to render

--- src/pages/packages/PackageContainer.jsx
+++ src/pages/packages/PackageContainer.jsx
@@ -4,52 +4,55 @@ import { PackageItem } from './components/PackageItem'
 import { useEffect, useState } from 'react'
 import Accordion from '../../components/Accordion'
 import rightLargeIcon from '../../assets/svg/rightLarge.svg'
+import { useTranslation } from 'react-i18next'
 
 export default function PackageContainer() {
+  const { t } = useTranslation()
+
   const [packagItems, setPakageItems] = useState()
   const [focusedPackage, setFocusedPackage] = useState()
+  const [focusedPackageTrajectory, setFocusedPackageTrajectory] = useState()
 
   useEffect(() => {
     setPakageItems(getPackageItems())
     setFocusedPackage(getPackageItems()[0])
+    setFocusedPackageTrajectory(getPackageItems()[0].trajectory[0])
   }, [])
 
-  const handleClickPackage = (item) => {
-    setFocusedPackage(item)
-  }
+  const handleClickPackage = (item) => setFocusedPackage(item)
 
   return (
     <div className="packageContainer">
       <section className="packageContainer__descriptions-fields">
         <div className="packageContainer__title">
           <h2>
-            <span className="fixed-span">UNFORGETTABLE TOURS</span>
+            <span className="fixed-span">{t('UNFORGETTABLE TOURS')}</span>
             <span className="iconRight">
-              ARE <img src={rightLargeIcon} alt="icon" />
+              {t('ARE')} <img src={rightLargeIcon} alt="icon" />
             </span>
-            <span>WAITING FOR</span>
-            <span className="you">YOU</span>
+            <span>{t('WAITING FOR')}</span>
+            <span className="you">{t('YOU')}</span>
           </h2>
         </div>
         <div className="packageContainer__description">
-          <h3>Tour package 7 days</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            voluptatum, quibusdam, quia, voluptates voluptate quod quos
-            voluptatibus doloribus quae quas fugit. Quisquam voluptatum,
-          </p>
-          <button>CONTACT AGENT</button>
+          <h3>
+            {focusedPackage?.title}
+            <br />
+            <span>{focusedPackage?.duration}</span>
+          </h3>
+          <p>{t('Package description')}</p>
+          <button>{t('contact agent')}</button>
         </div>
       </section>
       <section className="packageContainer__focused-package">
         <div className="packageContainer__focused-package__image">
-          <img src={focusedPackage?.image} alt="" />
-          <h3>Name site</h3>
+          <img src={focusedPackageTrajectory?.image} alt="package" />
+          <h3>{focusedPackageTrajectory?.site}</h3>
         </div>
       </section>
       <section className="packageContainer__packages">
         <div className="content_package">
-          <h3>Other Packages</h3>
+          <h3>{t('Other Packages')}</h3>
           <div className="content_package__items">
             {packagItems?.map((item) => (
               <PackageItem
@@ -63,18 +66,18 @@ export default function PackageContainer() {
         </div>
 
         <div className="content_package-trajectory">
-          <h3>Trajectory</h3>
+          <h3>{t('Trajectory')}</h3>
           <div className="content_package-trajectory-focused__items">
             {focusedPackage?.trajectory?.map((item, index) => (
               <Accordion
                 key={index}
-                title={item.title}
-                subtitle={item.route}
+                title={t(item.title) + ' ' + (index + 1)}
+                subtitle={t(item.route)}
                 description={item.description}
-                site={item.site}
+                site={t(item.site)}
                 enableButton={true}
-                titleButton={'BOOK NOW'}
-                onClickedButton={() => console.log('clicked')}
+                titleButton={t('contact agent')}
+                onClickedButton={() => setFocusedPackageTrajectory(item)}
               />
             ))}
           </div>
